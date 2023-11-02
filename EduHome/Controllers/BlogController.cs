9a2b995fbdd881220b3dba9dce9f7b7710d0ac7b
IsaSadigovDV <isans@code.edu.az>
2023-11-02 09:35:16 +0400

    project created

---
+++ EduHome/Controllers/BlogController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EduHome.Controllers
+{
+    public class BlogController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ EduHome/Data/EduHomeDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EduHome.Data
+{
+    public class EduHomeDbContext:DbContext
+    {
+        public EduHomeDbContext(DbContextOptions<EduHomeDbContext> options):base(options) { }
+    }
+}
