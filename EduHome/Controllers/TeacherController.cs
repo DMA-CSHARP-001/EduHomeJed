429f89af8547510c3b4be1488c52533d05942981
Ceko2004 <76591863+jalilverdiyev@users.noreply.github.com>
2023-11-16 11:15:43 +0400

    teacher details added

--- EduHome/Controllers/TeacherController.cs
+++ EduHome/Controllers/TeacherController.cs
@@ -1,12 +1,49 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EduHome.Data;
+using EduHome.Dtos.Teacher;
+using EduHome.Models;
+using EduHome.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace EduHome.Controllers
 {
     public class TeacherController : Controller
     {
-        public IActionResult Index()
+        EduHomeDbContext _dbContext;
+
+		public TeacherController(EduHomeDbContext dbContext)
+		{
+			_dbContext = dbContext;
+		}
+
+		public IActionResult Index()
         {
-            return View();
+
+            // select id, imagePath, firstName, lastName, degree from Teachers
+            var list = _dbContext.Teachers.Select(t => new IndexDto()
+            {
+                Id = t.Id,
+                Image = t.ImagePath,
+                FullName = t.FirstName + " " + t.LastName,
+				Degree = t.Degree
+            }).ToList();
+
+            TeacherVm teacherVm = new TeacherVm();
+            teacherVm.Teachers = list;
+
+            return View(teacherVm);
+        }
+
+        public IActionResult Detail(string teacherId)
+        {
+            // Select * from Teachers where id = teacherId
+            //var teacher = _dbContext.Teachers.FirstOrDefault(t => t.Id == Guid.Parse(teacherId));
+            //var teac = _dbContext.Teachers.Find(teacherId);
+            var teac = _dbContext.Teachers.Where(te => te.Id == Guid.Parse(teacherId))
+                                            .Include(t => t.Faculties)
+                                            .Include(t => t.Hobbies).FirstOrDefault();
+
+            return View(teac);
         }
     }
 }
---
+++ EduHome/ViewModels/TeacherVm.cs
@@ -0,0 +1,10 @@
+﻿using EduHome.Dtos.Teacher;
+using EduHome.Models;
+
+namespace EduHome.ViewModels
+{
+	public class TeacherVm
+	{
+        public List<IndexDto> Teachers { get; set; }
+    }
+}
